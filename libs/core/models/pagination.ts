24827ad7e21ee79da90c1881e4d817d8cab30533
AndreySkripachev <andrey.skripachev@saritasa.com>
2022-07-01 17:29:28 +0700

    Cleaning the core folder

--- libs/core/models/pagination.ts
+++
@@ -1,9 +0,0 @@
-import { Immerable, OmitImmerable } from "./immerable";
-
-/** Pagination. */
-export class Pagination extends Immerable {
-  public readonly totalBooks: number;
-  public readonly 
-}
-
-type PostInitArgs = OmitImmerable;
