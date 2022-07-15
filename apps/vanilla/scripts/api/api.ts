415a6bdddd1129ac5025c16b3ff056b7ac3ff378
AndreySkripachev <andrey.skripachev@saritasa.com>
2022-07-15 12:50:47 +0700

    Create registration page

--- apps/vanilla/scripts/api/api.ts
+++ apps/vanilla/scripts/api/api.ts
@@ -1,7 +1,10 @@
 import { AnimeApi } from './anime';
+import { UserApi } from './user';
 
 /** Receiving and sending data to the API. */
 export namespace Api {
 
   export const animeApi = new AnimeApi('anime/anime/');
+
+  export const userApi = new UserApi();
 }
