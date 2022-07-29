02e109f9df2644776ef2936964de2958852dba2b
AndreySkripachev <andrey.skripachev@saritasa.com>
2022-07-29 15:33:45 +0700

    Added path to auth in routing JC19-428

--- apps/angular/src/app/app-routing.module.ts
+++ apps/angular/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AnimeComponent } from './features/anime/anime.component';
+import { AuthComponent } from './features/auth/auth.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'catalog' },
   { path: 'catalog', component: AnimeComponent },
+  { path: 'auth', component: AuthComponent }
 ];
 
 /** App routing module. */
