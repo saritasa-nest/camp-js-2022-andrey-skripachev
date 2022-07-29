4ba3ed87445962d8ebb0572894f3f4baf2923ccc
AndreySkripachev <andrey.skripachev@saritasa.com>
2022-07-29 16:04:32 +0700

    Added redirect to authorization JC19-428

--- apps/angular/src/app/app-routing.module.ts
+++ apps/angular/src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { PathLocationStrategy, LocationStrategy } from '@angular/common';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
@@ -8,13 +8,13 @@ import { AuthComponent } from './features/auth/auth.component';
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'catalog' },
   { path: 'catalog', component: AnimeComponent },
-  { path: 'auth', component: AuthComponent }
+  { path: 'auth', component: AuthComponent },
 ];
 
 /** App routing module. */
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
-  providers: [{ provide: LocationStrategy, useClass: PathLocationStrategy }],
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
 })
 export class AppRoutingModule { }
