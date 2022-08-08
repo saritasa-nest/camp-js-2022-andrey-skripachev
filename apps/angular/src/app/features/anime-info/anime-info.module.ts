ee71c368666a7c2bfe6f5b78444d442de28e5594
AndreySkripachev <andrey.skripachev@saritasa.com>
2022-08-08 11:40:42 +0700

    Added displaying details for anime JC19-438

--- apps/angular/src/app/features/anime-info/anime-info.module.ts
+++ apps/angular/src/app/features/anime-info/anime-info.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { MatIconModule } from '@angular/material/icon';
 import { MatChipsModule } from '@angular/material/chips';
+import { MatListModule } from '@angular/material/list';
 
 import { SharedModule } from '../../../shared/shared.module';
 
@@ -22,6 +23,7 @@ import { AnimeViewComponent } from './anime-view/anime-view.component';
     SharedModule,
     MatIconModule,
     MatChipsModule,
+    MatListModule,
   ],
 })
 export class AnimeInfoModule {}
