50b11126b999d7fe11087c396185925546d03e61
Andrey <scrioachev@gmail.com>
2022-08-15 20:17:45 +0700

    Preparing for PR JC19-408

--- libs/core/enums/enums-functions.ts
+++
@@ -1,6 +0,0 @@
-export function reverseMapper<T, R>(mapper: Readonly<Record<T, R>>): Readonly<Record<R, T>> {
-  return Object.entries(mapper).map(([map, result]) => [result, map]);
-
-}
-
-export function elementInEnum<T>(value: string | number, currentEnum: T): 
