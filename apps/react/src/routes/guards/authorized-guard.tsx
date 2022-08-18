0bbae6f2eb508f6816cbbafa906697b29a23ba75
AndreySkripachev <andrey.skripachev@saritasa.com>
2022-08-18 17:16:28 +0700

    Fix interceptors JC19-458

--- apps/react/src/routes/guards/authorized-guard.tsx
+++ apps/react/src/routes/guards/authorized-guard.tsx
@@ -1,12 +1,25 @@
-import { selectUser } from '@js-camp/react/store/user/selectors';
-import { FC } from 'react';
+import { fetchUser } from '@js-camp/react/store/user/dispatchers';
+import { selectAreUserLoading, selectUser } from '@js-camp/react/store/user/selectors';
+import { FC, useEffect } from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 
-import { useAppSelector } from '../../store';
+import { AppLoadingSpinner } from '../../app/components/AppLoading';
+
+import { useAppDispatch, useAppSelector } from '../../store';
 
 export const AuthorizedGuard: FC = () => {
 
+  const appDispatch = useAppDispatch();
   const user = useAppSelector(selectUser);
+  const isLoading = useAppSelector(selectAreUserLoading);
+
+  useEffect(() => {
+    appDispatch(fetchUser());
+  }, [appDispatch]);
+
+  if (isLoading) {
+    return (<AppLoadingSpinner />);
+  }
 
   if (user !== null) {
     return <Navigate to="/genres" replace />;
