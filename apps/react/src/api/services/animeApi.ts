835210dfa9e81fed6ba00cc7546a291502c92c80
AndreySkripachev <andrey.skripachev@saritasa.com>
2022-08-19 08:51:24 +0700

    Added anime service JC19-468

---
+++ apps/react/src/api/services/animeApi.ts
@@ -0,0 +1,28 @@
+import { AnimeDto } from '@js-camp/core/dtos/anime.dto';
+import { PaginationDto } from '@js-camp/core/dtos/pagination.dto';
+import { AnimeMapper } from '@js-camp/core/mappers/anime.mapper';
+import { PaginationMapper } from '@js-camp/core/mappers/pagination.mapper';
+import { Anime } from '@js-camp/core/models/anime';
+import { Pagination } from '@js-camp/core/models/pagination';
+
+import { http } from '..';
+
+export namespace AnimeService {
+
+  const defaultSearchParams = new URLSearchParams({
+    limit: '10',
+    offset: '0',
+    ordering: 'id',
+  });
+
+  /**
+   * Gets pagination with first anime of anime list.
+   */
+  export async function getFirstPageOfAnimeList(): Promise<Pagination<Anime>> {
+    const animeResponse = await http.get<PaginationDto<AnimeDto>>('anime/anime/', {
+      params: defaultSearchParams,
+    });
+
+    return PaginationMapper.fromDto(animeResponse.data, AnimeMapper.fromDto);
+  }
+}
