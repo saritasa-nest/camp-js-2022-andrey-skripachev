73c4190fa4b6bb80eff06a5d0059279b3cebf993
Andrey <scrioachev@gmail.com>
2022-08-21 17:18:27 +0700

    Added filler for anime details JC19-468

--- apps/react/src/features/anime/components/AnimeDetails/AnimeDetails.tsx
+++ apps/react/src/features/anime/components/AnimeDetails/AnimeDetails.tsx
@@ -1,10 +1,22 @@
-import { Container } from '@mui/system';
+import { Box, Typography } from '@mui/material';
 import { FC, memo } from 'react';
+import CollectionsBookmarkIcon from '@mui/icons-material/CollectionsBookmark';
 
 const AnimeDetailsComponent: FC = () => (
-  <Container maxWidth='sm' sx={{
-    backgroundColor: '#eee',
-  }}></Container>
+  <Box sx={{
+    backgroundColor: '#eee6',
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: '#aaa',
+  }}>
+    <Typography component='p' variant='h4'>
+      The anime of your choice will be displayed here
+    </Typography>
+    <CollectionsBookmarkIcon fontSize='large' />
+  </Box>
 );
 
 export const AnimeDetails = memo(AnimeDetailsComponent);
