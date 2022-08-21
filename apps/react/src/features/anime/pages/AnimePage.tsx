ae6cc2c6259b94eb001251bbe2f930497dc3ca9d
Andrey <scrioachev@gmail.com>
2022-08-21 17:06:50 +0700

    Added beautiful scroll bar design JC19-468

--- apps/react/src/features/anime/pages/AnimePage.tsx
+++ apps/react/src/features/anime/pages/AnimePage.tsx
@@ -2,6 +2,7 @@ import { fetchAnimeList } from '@js-camp/react/store/animeList/dispatchers';
 import { useAppDispatch } from '@js-camp/react/store/store';
 import { Grid } from '@mui/material';
 import { FC, memo, useEffect } from 'react';
+import PerfectScrollbar from 'react-perfect-scrollbar';
 
 import { AppContent } from '../../../app/components/AppContent/AppContent';
 import { AppHeader } from '../../../app/components/AppHeader';
@@ -9,6 +10,7 @@ import { AppHeader } from '../../../app/components/AppHeader';
 import { AnimeDetails } from '../components/AnimeDetails';
 import { AnimeList } from '../components/AnimeList';
 
+import 'react-perfect-scrollbar/dist/css/styles.css';
 import './AnimePage.css';
 
 const AnimePageComponent: FC = () => {
@@ -23,7 +25,11 @@ const AnimePageComponent: FC = () => {
       <AppHeader />
       <AppContent>
         <Grid container className='full-height'>
-          <Grid item xs={4} className='full-height anime-control'><AnimeList /></Grid>
+          <Grid item xs={4} className='full-height anime-control'>
+            <PerfectScrollbar>
+              <AnimeList />
+            </PerfectScrollbar>
+          </Grid>
           <Grid item xs={8} className='full-height anime-control'><AnimeDetails /></Grid>
         </Grid>
       </AppContent>
