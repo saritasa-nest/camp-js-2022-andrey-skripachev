310b2ff9bb8969a7fcaab1fb16f99b1d8166e695
AndreySkripachev <andrey.skripachev@saritasa.com>
2022-08-23 17:21:35 +0700

    Add fetching anime by id JC19-488

--- apps/react/src/api/services/animeApi.ts
+++ apps/react/src/api/services/animeApi.ts
@@ -1,9 +1,12 @@
+import { AnimeDetailsDto } from '@js-camp/core/dtos/anime-details.dto';
 import { AnimeDto } from '@js-camp/core/dtos/anime.dto';
 import { PaginationDto } from '@js-camp/core/dtos/pagination.dto';
+import { AnimeDetailsMapper } from '@js-camp/core/mappers/anime-details.mapper';
 import { AnimeMapper } from '@js-camp/core/mappers/anime.mapper';
 import { PaginationMapper } from '@js-camp/core/mappers/pagination.mapper';
 import { QueryParamsMapper } from '@js-camp/core/mappers/query-params.mapper';
 import { Anime } from '@js-camp/core/models/anime';
+import { AnimeDetails } from '@js-camp/core/models/anime-details';
 import { Pagination } from '@js-camp/core/models/pagination';
 import { QueryParams } from '@js-camp/core/models/query-params';
 
@@ -33,5 +36,15 @@ export namespace AnimeService {
     });
 
     return PaginationMapper.fromDto(animeResponse.data, AnimeMapper.fromDto);
+  }
+
+  /**
+   * Fetches anime by id.
+   * @param id Anime id.
+   */
+  export async function getAnimeById(id: number): Promise<AnimeDetails> {
+    const animeResponse = await http.get<AnimeDetailsDto>(`anime/anime/${id}/`);
+
+    return AnimeDetailsMapper.fromDto(animeResponse.data);
   }
 }
